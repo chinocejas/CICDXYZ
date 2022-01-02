6c545d8729388b9aedce432a9b1e133abda16283
chinocejas <nicocejas@outlook.com>
2022-01-02 20:19:09 -0300

    creating new project (#1)

--- Providers/Functions/Information.cs
+++ Providers/Functions/Information.cs
@@ -7,14 +7,15 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Entities.Providers;
 
-namespace Providers
+namespace Providers.Functions
 {
-    public static class Function1
+    public static class Information
     {
-        [FunctionName("Function1")]
+        [FunctionName("Information")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -25,11 +26,9 @@ namespace Providers
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            var responseMessage = new PersonalInformation() { MyProperty = 1 };
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(new { responseMessage });
         }
     }
 }
