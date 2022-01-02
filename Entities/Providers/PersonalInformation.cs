77a301c7eb9488becf16c9e2950c8ec8fe8da423
chinocejas <nicocejas@outlook.com>
2022-01-02 20:18:32 -0300

    creating new project

---
+++ Entities/Providers/PersonalInformation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Providers
+{
+    public class PersonalInformation
+    {
+        public int MyProperty { get; set; }
+    }
+}
